atmin/jtmpl-core

commit 7e5fbe8a6cc91cf61335d78089a3533e08b3c637

small fixes + update bugfixed freak
Bugs observed while developing non-trivial application.

src/rules/value-var.js
@@ -16,17 +16,20 @@ Handle "value", "checked" and "selected" attributes
           node[attr] = val;
         }
       }
-      
+
       if (match && ['value', 'checked', 'selected'].indexOf(attr) > -1) {
         // <select> option?
         if (node.nodeName === 'OPTION') {
           // Attach async, as parentNode is still documentFragment
           setTimeout(function() {
-            node.parentNode.addEventListener('change', function() {
-              if (model(prop) !== node.selected) {
-                model(prop, node.selected);
-              }
-            });
+            if (node && node.parentNode && !node.parentNode.__value_var_change) {
+              node.parentNode.__value_var_change = true;
+              node.parentNode.addEventListener('change', function() {
+                if (model(prop) !== node.selected) {
+                  model(prop, node.selected);
+                }
+              });
+            }
           }, 0);
         }
 
@@ -34,7 +37,7 @@ Handle "value", "checked" and "selected" attributes
         if (node.type === 'radio' && node.name) {
           node.addEventListener('change', function() {
             if (node[attr]) {
-              for (var i = 0, 
+              for (var i = 0,
                   inputs = document.querySelectorAll('input[type=radio][name=' + node.name + ']'),
                   len = inputs.length;
                   i < len;

src/compiler.js
@@ -185,6 +185,10 @@ Return documentFragment
                     }
                   }
 
+                  if (rule.replace !== undefined) {
+                    attr.value = rule.replace;
+                  }
+
                   if (rule.change) {
                     model.on('change', rule.block || rule.prop, rule.change);
                     rule.change();

commit 0c9dd4f59fff19b4fa8bf10b074d59469db828e9

Bugfix, bug introduced in 7e5fbe8
Didn't attach event handlers to all <select> options.
Needs automatic test for this.

jtmpl-core.js
@@ -1246,8 +1246,7 @@ Handle "value", "checked" and "selected" attributes
         if (node.nodeName === 'OPTION') {
           // Attach async, as parentNode is still documentFragment
           setTimeout(function() {
-            if (node && node.parentNode && !node.parentNode.__value_var_change) {
-              node.parentNode.__value_var_change = true;
+            if (node && node.parentNode) {
               node.parentNode.addEventListener('change', function() {
                 if (model(prop) !== node.selected) {
                   model(prop, node.selected);

src/rules/value-var.js
@@ -22,8 +22,7 @@ Handle "value", "checked" and "selected" attributes
         if (node.nodeName === 'OPTION') {
           // Attach async, as parentNode is still documentFragment
           setTimeout(function() {
-            if (node && node.parentNode && !node.parentNode.__value_var_change) {
-              node.parentNode.__value_var_change = true;
+            if (node && node.parentNode) {
               node.parentNode.addEventListener('change', function() {
                 if (model(prop) !== node.selected) {
                   model(prop, node.selected);