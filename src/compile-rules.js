0712f4fc9d1c5de83c6e8bcb1dd881b0e9f75646
Atanas Minev <fr33rider@gmail.com>
2014-10-22 23:20:08 +0300

    Attribute rules, placeholders

--- src/compile-rules.js
+++ src/compile-rules.js
@@ -1,3 +1,15 @@
+// TODO: REFACTORME
+
+jtmpl.get = function(model, prop) {
+  var val = model(prop);
+  return (typeof val === 'function') ?
+    JSON.stringify(val.values) :
+    val;
+};
+
+
+
+
 /**
  * Rules
  */
@@ -9,10 +21,76 @@ module.exports = {
 
   attr: [
 
+
+    /**
+     * selected="{{var}}"
+     */
     function(node, attr) {
 
+    },
+
+
+
+
+    /**
+     * checked="{{var}}"
+     */
+    function(node, attr) {
+
+    },
+
+
+
+
+    /**
+     * class="{{var}}"
+     */
+    function(node, attr) {
+
+    },
+
+
+
+
+    /**
+     * style="{{var}}"
+     */
+    function(node, attr) {
+
+    },
+
+
+
+
+    /**
+     * attribute="{{var}}"
+     */
+    function(node, attr) {
+      var match = node.getAttribute(attr).match(/^\{\{([\w\.\-]+)\}\}$/);
+      if (match) {
+
+        return {
+
+          prop: match[1],
+
+          rule: function(node, attr, model, prop) {
+
+            function change() {
+              var val = jtmpl.get(model, prop);
+              return val ?
+                node.setAttribute(attr, val) :
+                node.removeAttribute(attr);
+            }
+
+            model.on('change', prop, change);
+            change();
+          }
+        };
+      }
     }
   ],
+
+
 
 
 
@@ -25,18 +103,45 @@ module.exports = {
      */
     function(node) {
       if (node.innerHTML.match(/^[\w\.\-]+$/)) {
+
         return {
+
           prop: node.innerHTML,
-          rule: function(fragment, prop, model) {
+
+          rule: function(fragment, model, prop) {
             var textNode = document.createTextNode(model(prop) || '');
             fragment.appendChild(textNode);
             model.on('change', prop, function() {
-              textNode.data = model(prop) || '';
+              textNode.data = jtmpl.get(model, prop) || '';
             });
           }
         };
       }
     },
+
+
+
+
+    /**
+     * {{&var}}
+     */
+    function(node) {
+      if (node.innerHTML.match(/^&[\w\.\-]+$/)) {
+      }
+    },
+
+
+
+
+    /**
+     * {{>partial}}
+     */
+    function(node) {
+      // match: [1]=var_name, [2]='single-quoted' [3]="doube-quoted"
+      if (node.innerHTML.match(/>([\w\.\-]+)|'([^\']*)\'|"([^"]*)"/)) {
+      }
+    },
+
 
 
 
@@ -52,7 +157,7 @@ module.exports = {
 
           block: match[1],
 
-          rule: function(fragment, prop, model, template) {
+          rule: function(fragment, model, prop, template) {
 
             // Anchor node for keeping section location
             var anchor = document.createComment('');
@@ -67,6 +172,7 @@ module.exports = {
                 var anchorIndex = [].indexOf.call(parent.childNodes, anchor);
                 var pos = anchorIndex - length + i * chunkSize;
 
+                // TODO: fixme, this is not correct!!!
                 parent.replaceChild(
                   eval(template + '(model(prop)(i))'),
                   parent.childNodes[pos]
@@ -153,6 +259,20 @@ module.exports = {
             change();
           }
         };
+      }
+    },
+
+
+
+
+
+    /**
+     * {{^inverted_section}}
+     */
+    function(node) {
+      var match = node.innerHTML.match(/^\^([\w\.\-])+$/);
+
+      if (match) {
       }
     }
 
--- src/compile.js
+++ src/compile.js
@@ -10,12 +10,19 @@ function compile(template, sourceURL) {
 
   // Compile rules, for attributes and nodes
   var compileRules = require('./compile-rules');
+  var ri, rules, rlen;
   var match, block;
 
   // Generate dynamic function body
   var func = '(function(model) {\n' +
     'var frag = document.createDocumentFragment(), node;\n\n';
 
+  // Wrap model in a Freak instance, if necessary
+  func += 'model = typeof model === "function" ?' +
+    'model : ' +
+    'typeof model === "object" ?' +
+      'jtmpl(model) :' +
+      'jtmpl({".": model});\n\n';
 
   // Iterate childNodes
   for (var i = 0, childNodes = template.childNodes, len = childNodes.length, node;
@@ -31,8 +38,9 @@ function compile(template, sourceURL) {
         // jtmpl tag?
         if (node.nodeName === 'SCRIPT' && node.type === 'text/jtmpl-tag') {
 
-          for (var ri = 0, rules = compileRules.node, rlen = rules.length;
+          for (ri = 0, rules = compileRules.node, rlen = rules.length;
               ri < rlen; ri++) {
+
             match = rules[ri](node);
 
             // Rule found?
@@ -54,17 +62,16 @@ function compile(template, sourceURL) {
                 }
                 else {
                   func += '(' + match.rule.toString() + ')' +
-                    '(frag, ' +
-                    JSON.stringify(match.block) +
-                    ', model, ' +
-                    JSON.stringify(compile(block)) + ');';
+                    '(frag, model, ' +
+                    JSON.stringify(match.block) + ', ' +   // prop
+                    JSON.stringify(compile(block)) + ');'; // template
                 }
 
               }
               // Inline tag
               else {
                 func += '(' + match.rule.toString() + ')' +
-                  '(frag, ' + JSON.stringify(match.prop) + ', model);\n';
+                  '(frag, model, ' + JSON.stringify(match.prop) + ');\n';
               }
 
               // Skip remaining rules
@@ -83,15 +90,44 @@ function compile(template, sourceURL) {
           // Create element
           func += 'node = document.createElement("' + node.nodeName + '");\n';
 
-          // Clone attributes
+          // Process attributes
           for (var ai = 0, attributes = node.attributes, alen = attributes.length;
                ai < alen; ai++) {
-                 func += 'node.setAttribute("' +
-                   attributes[ai].name +
-                   '", ' +
-                   JSON.stringify(attributes[ai].value) +
-                   ');\n';
-               }
+
+            if (attributes[ai].value.match(/\{\{/)) {
+
+              // Opening delimiter found, process attribute rules
+              for (ri = 0, rules = compileRules.attr, rlen = rules.length;
+                  ri < rlen; ri++) {
+
+                match = rules[ri](node, attributes[ai].name);
+
+                if (match) {
+
+                  // Match found, append rule to func
+                  func += '(' + match.rule.toString() + ')' +
+                    '(node, ' +
+                    JSON.stringify(attributes[ai].name) + // attr
+                    ', model, ' +
+                    JSON.stringify(match.prop) +          // prop
+                    ');\n';
+
+                  // Skip other attribute rules
+                  break;
+                }
+              }
+
+            }
+            else {
+
+              // Just clone the attributes
+              func += 'node.setAttribute("' +
+                attributes[ai].name +
+                '", ' +
+                JSON.stringify(attributes[ai].value) +
+                ');\n';
+            }
+          }
 
           // Recursively compile
           func += 'node.appendChild(' + compile(node) + '());\n';
